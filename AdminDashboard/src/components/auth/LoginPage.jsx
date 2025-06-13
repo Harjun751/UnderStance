d85d9df61bfcdd88d44db7a2042d0207b066ef31
Harjun751 <Harjun751@gmail.com>
2025-06-14 00:55:07 +0800

    Updated login page, added small test, updated CI scripts to include

--- AdminDashboard/src/components/auth/LoginPage.jsx
+++ AdminDashboard/src/components/auth/LoginPage.jsx
@@ -1,19 +1,32 @@
 import { useState } from 'react'
 import "./LoginPage.css";
+import LoginButton from "./LoginButton";
+import Title from "../general/Title";
 
 export default () => {
   return (
     <div id="login">
-        <img id="hero" src="../../../src/assets/hdb.jpg" />
+        <img id="hero" src="../../../src/assets/hdb.jpg" alt="Singapore Apartment Buildings" />
         <form>
-            <h1 id="title">
-                <span>Under</span>
-                <span color="var(--red)">Stance</span>
-            </h1>
-            <label for="email">Email</label>
-            <input name="email" id="email" placeholder="email@email.com"/>
-            <label for="password">Password</label>
-            <input name="password" id="password" placeholder="sick-password"/>
+            <Title />
+            <div class="input-group">
+                <label for="email">Email</label>
+                <input name="email" id="email" type="email" placeholder="email@email.com"/>
+            </div>
+            <div class="input-group">
+                <label for="password">Password</label>
+                <input name="password" id="password" placeholder="sick-password" type="password" />
+           </div>
+           <div class="input-group checkbox">
+                <label for="remember-me">Remember me: </label>
+                <input type="checkbox" name="remember-me" id="remember-me" />
+           </div>
+           <div class="input-group">
+                <LoginButton />
+           </div>
+           <div class="input-group">
+            <a href="/">Reset Password</a>
+              </div>
         </form>
     </div>
   );
--- AdminDashboard/vite.config.js
+++ AdminDashboard/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    test: {
+        environment: "jsdom",
+        globals: true,
+        css: true,
+        setupFiles: "./src/tests/setup.jsx",
+        reporters: ["default", "junit"],
+        outputFile: {
+            junit: "./coverage/results.xml",
+        },
+    }
 })
