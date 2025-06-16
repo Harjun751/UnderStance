5dd72f1c772f115de541d5560aa96de7fec5f4cb
Arjun <57029562+Harjun751@users.noreply.github.com>
2025-06-16 20:30:32 +0800

    Arjun/jira 34 create admin dashboard (#22)
    * Created scaffold for app and edited compose
    
    * Oops: added the dashboard files
    
    * WIP on the login page
    
    * Updated login page, added small test, updated CI scripts to include
    
    * Prettified code 💅
    
    * Some style changes for FE
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ AdminDashboard/src/components/auth/LoginButton.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./LoginButton.css";
+
+export default () => {
+    return (
+        <>
+            <button id="login-button" type="submit">
+                Login
+            </button>
+        </>
+    );
+};
