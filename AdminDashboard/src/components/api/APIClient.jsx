c68d4e86fc8461c5f5d10265e93d985db314b6db
Harjun751 <Harjun751@gmail.com>
2025-06-27 10:56:18 +0800

    Added all endpoint actions for api client

--- AdminDashboard/src/components/api/APIClient.jsx
+++ AdminDashboard/src/components/api/APIClient.jsx
@@ -1,70 +1,5 @@
 import axios from "axios";
 
-/*async function fetchWithCreds(url, method, data) {
-    const { getAccessTokenSilently } = useAuth0();
-    const token = await getAccessTokenSilently({
-        authorizationParams: {
-            audience: "https://understance-backend.onrender.com/",
-        },
-    });
-    const token = "hi";
-
-    const headers =  {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': "application/json"
-    }
-    const options = (data === null) ?
-        { method: method, headers: headers }
-     :  { method: method, headers: headers, body: JSON.stringify(data) }
-
-    const res = await fetch(url, options);
-    return await res.json();
-}
-
-
-export const getQuestions = () => {
-    return fetchWithCreds(
-        `${import.meta.env.VITE_API_URL}/questions`,
-        "GET",
-        null
-    )
-}
-
-export const postQuestion = (description, summary, categoryID, active) => {
-    return fetchWithCreds(
-        `${import.meta.env.VITE_API_URL}/questions`,
-        "POST",
-        {
-            Description: description,
-            Summary: summary,
-            categoryID: categoryID,
-            active: active
-        }
-    )
-}
-
-export const putQuestion = (id, description, summary, categoryID, active) => {
-    return fetchWithCreds(
-        `${import.meta.env.VITE_API_URL}/questions`,
-        "POST",
-        {
-            IssueID: id,
-            Description: description,
-            Summary: summary,
-            categoryID: categoryID,
-            active: active
-        }
-    )
-}
-
-export const deleteQuestion = (id) => {
-    return fetchWithCreds(
-        `${import.meta.env.VITE_API_URL}/questions/${id}`,
-        "DELETE",
-        null
-    )
-}*/
-
 export const createAPIClient = (getAccessTokenSilently) => {
     const client = axios.create({
         baseURL: `${import.meta.env.VITE_API_URL}`,
@@ -88,16 +23,108 @@ class APIClientWrapper {
         this.client = axiosClient;
     }
 
+    getQuestions() {
+        return this.client.get("/questions").then((resp) => resp.data);
+    }
+
+    addQuestion(description, summary, categoryID, active) {
+        return this.client.post("/questions, {
+            Description: description,
+            Summary: summary,
+            CategoryID: categoryID,
+            active: active
+        }
+    }
+
+    updateQuestion(id, description, summary, categoryID, active) {
+        return this.client.put("/questions, {
+            IssueID: id,
+            Description: description,
+            Summary: summary,
+            CategoryID: categoryID,
+            active: active
+        }
+    }
+
+    deleteQuestion(id) {
+        return this.client.delete(`/questions/${id}`);
+    }
+
+
     getCategories() {
         return this.client.get("/categories").then((resp) => resp.data);
     }
+
+    addCategory(name) {
+        return this.client.post("/categories", {
+            Name: name
+        }
+    }
+
+    updateCategory(id, name) {
+        return this.client.put("/categories", {
+            Name: name,
+            CategoryID: id
+        }
+    }
+
+    deleteCategory(id) {
+        return this.client.delete(`/categories/${id}`);
+    }
+
     getParties() {
         return this.client.get("/parties").then((resp) => resp.data);
     }
+
+    addParty(name, shortName, icon, partyColor, active) {
+        return this.client.post("/parties", {
+            Name: name,
+            ShortName: shortName,
+            Icon: icon,
+            PartyColor: partyColor,
+            Active: active
+        }
+    }
+
+    updateParty(id, name, shortName, icon, partyColor, active) {
+        return this.client.put("/parties", {
+            PartyID: id,
+            Name: name,
+            ShortName: shortName,
+            Icon: icon,
+            PartyColor: partyColor,
+            Active: active
+        }
+    }
+
+    deleteParty(id) {
+        return this.client.delete(`/parties/${id}`);
+    }
+
     getStances() {
         return this.client.get("/stances").then((resp) => resp.data);
     }
-    getQuestions() {
-        return this.client.get("/questions").then((resp) => resp.data);
+
+    addStance(stand, reason, issueID, partyID) {
+        return this.client.post("/stances", {
+            Stand: stand,
+            Reason: reason,
+            IssueID: issueID,
+            PartyID: partyID
+        }
+    }
+
+    updateStance(id, stand, reason, issueID, partyID) {
+        return this.client.put("/stances", {
+            StanceID: id,
+            Stand: stand,
+            Reason: reason,
+            IssueID: issueID,
+            PartyID: partyID
+        }
+    }
+
+    deleteStance(id) {
+        return this.client.delete(`/stances/${id}`);
     }
 }
