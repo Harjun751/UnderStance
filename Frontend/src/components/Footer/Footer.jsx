f6a9d1fcbc94eddfb624013b233e45ac2a69a981
Harjun751 <Harjun751@gmail.com>
2025-06-30 13:52:17 +0800

    Updated footer links

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -60,7 +60,7 @@ const Footer = () => {
                             <Link>
                                 <li>
                                     <a
-                                        href="https://www.youtube.com/watch?v=KZoipAb2fo4&list=RDKZoipAb2fo4&start_radio=1&pp=ygUOYnJhaW5yb3QgYW5pbWWgBwE%3D"
+                                        href="https://docs.google.com/spreadsheets/d/1amv7Aa4VcDngvbswcU4n80nv4tjPsn8oJAOVqbWXiyI/edit?usp=drive_link"
                                         target="_blank"
                                         rel="noopener noreferrer"
                                         class="footer-link"
@@ -73,38 +73,7 @@ const Footer = () => {
                     </div>
 
                     <div class="footer-column">
-                        <h3 class="footer-heading">Legal</h3>
-                        <ul class="footer-list">
-                            {/* add actual link & page next time, for now enjoy what i've left behind */}
-                            <Link>
-                                <li>
-                                    <a
-                                        href="https://www.youtube.com/watch?v=OyDyOweu-PA&pp=ygUKcG9saXNoIGNvdw%3D%3D"
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        class="footer-link"
-                                    >
-                                        Privacy
-                                    </a>
-                                </li>
-                            </Link>
-                            <Link>
-                                <li>
-                                    <a
-                                        href="https://www.youtube.com/watch?v=wRRsXxE1KVY&pp=ygUUam9obiBjZW5hIHByYW5rIGNhbGw%3D"
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        class="footer-link"
-                                    >
-                                        Terms
-                                    </a>
-                                </li>
-                            </Link>
-                        </ul>
-                    </div>
-
-                    <div class="footer-column">
-                        <h3 class="footer-heading">Connect</h3>
+                        <h3 class="footer-heading">Project Resources</h3>
                         <div class="footer-social">
                             <a
                                 href="https://github.com/Harjun751/UnderStance"
@@ -114,9 +83,8 @@ const Footer = () => {
                             >
                                 <FaGithub />
                             </a>
-                            {/* add actual link next time, for now enjoy what i've left behind */}
                             <a
-                                href="https://www.youtube.com/watch?v=fWNaR-rxAic&pp=0gcJCdgAo7VqN5tD"
+                                href="https://drive.google.com/drive/folders/1ng0SbyCBXA9R_t_X6sa0VSiRyDYCwJeu?usp=drive_link"
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 class="footer-icon"
@@ -124,7 +92,7 @@ const Footer = () => {
                                 <FaGoogleDrive />
                             </a>
                             <a
-                                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                                href="https://drive.google.com/file/d/15bZYKrSL7GOCpdxfzyzgFJd1GSbpPT2r/view?usp=drive_link"
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 class="footer-icon"
@@ -134,17 +102,13 @@ const Footer = () => {
                         </div>
                         <div class="footer-comment">
                             <p class="footer-note">
-                                HEY I JUST MET YOU <br />
-                                AND THIS IS CRAZY <br />
-                                BUT HERE'S MY NUMBER <br />
-                                SO CALL ME MAYBE
                             </p>
                         </div>
                     </div>
                 </div>
 
                 <div class="footer-bottom">
-                    <p>&copy; 2025 UnderStance</p>
+                    <p>2025 UnderStance</p>
                 </div>
             </div>
         </footer>
