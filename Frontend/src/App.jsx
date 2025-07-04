26d22135fdf60a3acd194c50d832eb6410e3b3e6
Thaddaeus Chua <thadchua@gmail.com>
2025-07-04 18:43:00 +0800

    Added google analytics tracking

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,22 @@
 //import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./components/HomePage/Home";
 import ReadStances from "./components/ReadStancesPage/ReadStances";
 import Quiz from "./components/QuizPage/Quiz";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import "./App.css";
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
 
 function App() {
+    const location = useLocation();
+
+    //Send a pageview on route change
+    useEffect(() => {
+        ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
+    }, [location]);
+    
     return (
         <Router>
             <div className="app-container">
